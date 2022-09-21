13b0768b57b06039099afe0fb9cce275a224a2de
v4lomyr <bintangdwitama160501@gmail.com>
2022-09-21 09:55:48 +0700

    🐱‍💻 Form Layout

--- lib/main.dart
+++ lib/main.dart
@@ -13,103 +13,228 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Scaffold(
+        body: FormContainer(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class FormContainer extends StatelessWidget {
+  const FormContainer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Container(
+      color: Colors.black,
+      margin: const EdgeInsets.all(32),
+      child: Container(
+        color: Colors.red,
+        margin: const EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+                flex: 3,
+                child: Container(
+                  alignment: Alignment.center,
+                  height: double.infinity,
+                  margin: const EdgeInsets.symmetric(),
+                  color: Colors.green,
+                  child: Container(
+                    alignment: Alignment.center,
+                    color: Colors.pink,
+                    margin: const EdgeInsets.all(16),
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Input Judul'
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 16),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Input Abstract'
+                              ),
+                              minLines: 16,
+                              maxLines: 16,
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Input Gold Keyphrase'
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ),
+                )
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+                flex: 2,
+                child: Container(
+                  alignment: Alignment.center,
+                  height: double.infinity,
+                  margin: const EdgeInsets.symmetric(),
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        margin: const EdgeInsets.only(top: 32),
+                        child: const Text(
+                          "Select Method",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                        child: const MethodRadio(),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 16),
+                        alignment: Alignment.center,
+                        child: ElevatedButton(
+                          onPressed: () {  },
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(88, 50)
+                          ),
+                          child: const Text("Generate Keyphrase"),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        margin: const EdgeInsets.only(top: 32),
+                        child: const Text(
+                          "Result : ",
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        margin: const EdgeInsets.only(top: 32),
+                        child: const Text(
+                          "Hasil 1, Hasil 2, Hasil 3, Hasil 4, Hasil 5",
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                )
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class MethodRadio extends StatefulWidget {
+  const MethodRadio({Key? key}) : super(key: key);
+
+  @override
+  State<MethodRadio> createState() => _MethodRadioState();
+}
+
+class _MethodRadioState extends State<MethodRadio> {
+  KeyphraseExtractionMethod? _keyphraseExtractionMethod = KeyphraseExtractionMethod.positionRank;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Column(
+            children: [
+              ListTile(
+                title: const Text('YAKE'),
+                leading: Radio<KeyphraseExtractionMethod>(
+                  value: KeyphraseExtractionMethod.yake,
+                  groupValue: _keyphraseExtractionMethod,
+                  onChanged: (KeyphraseExtractionMethod? value) {
+                    setState(() {
+                      _keyphraseExtractionMethod = value;
+                    });
+                  },
+                ),
+              ),
+              ListTile(
+                title: const Text(
+                    'TextRank',
+                  softWrap: false,
+                ),
+                leading: Radio<KeyphraseExtractionMethod>(
+                  value: KeyphraseExtractionMethod.textRank,
+                  groupValue: _keyphraseExtractionMethod,
+                  onChanged: (KeyphraseExtractionMethod? value) {
+                    setState(() {
+                      _keyphraseExtractionMethod = value;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Column(
+            children: [
+              ListTile(
+                title: const Text('PositionRank'),
+                leading: Radio<KeyphraseExtractionMethod>(
+                  value: KeyphraseExtractionMethod.positionRank,
+                  groupValue: _keyphraseExtractionMethod,
+                  onChanged: (KeyphraseExtractionMethod? value) {
+                    setState(() {
+                      _keyphraseExtractionMethod = value;
+                    });
+                  },
+                ),
+              ),
+              ListTile(
+                title: const Text('TopicRank'),
+                leading: Radio<KeyphraseExtractionMethod>(
+                  value: KeyphraseExtractionMethod.topicRank,
+                  groupValue: _keyphraseExtractionMethod,
+                  onChanged: (KeyphraseExtractionMethod? value) {
+                    setState(() {
+                      _keyphraseExtractionMethod = value;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+enum KeyphraseExtractionMethod { yake, textRank, positionRank, topicRank }
